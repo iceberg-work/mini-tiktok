6e7e9a36a2f7c77e4b73e94aa2dbef2b5c7ae329
iceberg-work <iceberg-work@qq.com>
2023-02-07 15:20:14 +0800

    进一步完善项目结构

--- cmd/mini-tiktok/main.go
+++ cmd/mini-tiktok/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	mini_tiktok "github.com/iceberg-work/mini-tiktok/internal/mini-tiktok"
+	"os"
 
+	_ "go.uber.org/automaxprocs"
+)
+
+// Go 程序的默认入口函数(主函数).
 func main() {
-	fmt.Println("!")
+	command := mini_tiktok.NewMiniBlogCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
---
+++ configs/mini-tiktok.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80030
+ Source Host           : localhost:3306
+ Source Schema         : mini-tiktok
+
+ Target Server Type    : MySQL
+ Target Server Version : 80030
+ File Encoding         : 65001
+
+ Date: 07/02/2023 14:28:09
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
